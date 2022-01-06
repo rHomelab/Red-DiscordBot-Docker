8f84259f849bc159a500df54c94aec85b9a21e90
tigattack <10629864+tigattack@users.noreply.github.com>
2022-01-06 23:00:28 +0000

    feat: add start script
    also creates a bot instance if none exists

---
+++ redbot.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -e
+
+# Check if a Red instance exists
+if redbot --list-instances | grep -qe '^No instances have been configured!'; then
+    echo "No Red instance found. Creating instance '$INSTANCE_NAME'..."
+    
+    SETUP_ARGS="--instance-name $INSTANCE_NAME --no-prompt --data-path /redbot/data"
+    
+    {
+        xargs redbot-setup <<< "$SETUP_ARGS" && \
+        echo "Red instance '$INSTANCE_NAME' created successfully."
+        } || {
+        echo "Failed to create Red instance '$INSTANCE_NAME'."
+        exit 1
+    }
+fi
+
+ARGS="$INSTANCE_NAME --token $TOKEN --prefix $PREFIX --no-prompt"
+
+if [ "$RPC_ENABLED" = 'true' ] && [ -z "$RPC_PORT" ]; then
+    ARGS="$ARGS --rpc"
+    
+    if [ -z "$RPC_PORT" ]; then
+        ARGS="$ARGS --rpc-port 6133"
+    fi
+fi
+
+if [ -n "$EXTRA_ARGS" ]; then
+    ARGS="$ARGS $EXTRA_ARGS"
+fi
+
+if [ "$TEAM_MEMBERS_ARE_OWNERS" = 'true' ]; then
+    ARGS="$ARGS --team-members-are-owners"
+fi
+
+# Start Red
+exec xargs redbot <<< "${ARGS}"
