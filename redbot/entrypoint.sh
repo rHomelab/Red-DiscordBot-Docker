993b7a5a014b0f066240155faff0254b2dbb1259
kiweezi <46600706+kiweezi@users.noreply.github.com>
2024-03-28 16:16:32 +0000

    Remove old expansion

--- redbot/entrypoint.sh
+++ redbot/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 chown -R ${RED_USER}:${RED_USER} ${RED_HOME}/data
 
 # Install pip packages and start Red as specified user
-exec runuser -u $RED_USER -- ${RED_HOME}/start-red.sh "$@"
+exec runuser -u $RED_USER -- ${RED_HOME}/start-red.sh $@
